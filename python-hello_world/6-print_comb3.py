caae58a9d6b4d9c630c4742333870f7c3ff6be5d
Ajala Dolapo <supremeajala@gmail.com>
2023-07-26 21:57:41 +0100

    last change

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i + 1, 10):
-        print("{:02d}".format(i), "{:02d}".format(j), end=", " if i < 8 or j < 9 else "\n")
+for i in range(0,10):
+    for j in range(0,10):
+        if (i != j) or (i):
+            print("{}{}, ".format(i,j), end="")
+
+
