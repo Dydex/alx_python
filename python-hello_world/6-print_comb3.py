6c509e852cc9603bd8174599a98869928f9e653e
Ajala Dolapo <supremeajala@gmail.com>
2023-07-26 22:26:04 +0100

    last change

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 for first_digit in range(0, 9):
     for second_digit in range(first_digit + 1, 10):
-        print(f"{first_digit:0d}{second_digit:0d}", end=",")
-  # Manually add 89 to the end, as it's the last combination
+        print("{:0d}{:0d}".format(first_digit,second_digit), end=",")
+  
 
 
 
