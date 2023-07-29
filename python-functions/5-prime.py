26013eb2afdc8b85f06c997b0081aeafd1d5389a
Ajala Dolapo <supremeajala@gmail.com>
2023-07-29 14:27:18 +0100

    sixth task

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -3,3 +3,10 @@ def is_prime(number):
         return False
     elif number == 2:
         return True
+
+    # Check for divisibility by numbers from 2 up to the square root of the number
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            return False
+
+    return True
