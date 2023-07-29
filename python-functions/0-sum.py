527ffd7889295f84d798501bb2110fa3a55f985b
Ajala Dolapo <supremeajala@gmail.com>
2023-07-29 12:35:30 +0100

    first task

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,18 +1,9 @@
 #!/usr/bin/env python3
 def add(x, y):
     return x + y
+
 print(add(1,2))
-
-def add(x, y):
-    return x + y
 print(add(100,-2))
-
-def add(x, y):
-    return x + y
 print(add(-100,-2))
-
-def add(x, y):
-    return x + y
 print(add(0,0))
-
 
