9a12d52c71631a3574999ee26d84aee12bd88c25
Ajala Dolapo <supremeajala@gmail.com>
2023-07-29 09:04:41 +0100

    last change

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-for first_digit in range(0, 9):
-    for second_digit in range(first_digit + 1, 10):
-        print("{}{}".format(first_digit,second_digit), end=",")
-  
-
-
-
+for x in range(10):
+    for y in range(x + 1, 10):
+        if x < 8:
+            print("{:0d}{:0d}".format(x, y), end=", ")
+        else:
+            print("{:0d}{:0d}".format(x, y))
+            
