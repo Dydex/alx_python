c8eca137e37f4e0cbcbb288d86dc0178aa3d1a65
Ajala Dolapo <supremeajala@gmail.com>
2023-07-29 12:30:23 +0100

    first task

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-add = __import__('0-sum').add
 def add(x, y):
     return x + y
 print(add(1,2))
-print(add(98,0))
 print(add(100,-2))
+print(add(-100,-2))
+print(add(0,0))
+
 
