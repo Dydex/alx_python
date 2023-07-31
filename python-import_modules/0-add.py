13f897d4c9c9ccb85853d6459f749ab9bb21f736
Ajala Dolapo <supremeajala@gmail.com>
2023-07-31 21:10:10 +0100

    first task

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-#from add_0 import sum
+
 from add_0 import sub
 
 if __name__== "__main__":
@@ -8,7 +8,6 @@ if __name__== "__main__":
     a = 1
     b = 2
 
-    result = sum(a,b)
     result = sub(a,b)
     print("{}+{}={}".format(a,b,result))
 
