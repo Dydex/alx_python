61834f6d9e667d0663abc0932d63c319c4e5b8a6
Ajala Dolapo <supremeajala@gmail.com>
2023-07-31 23:38:41 +0100

    first task

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -3,10 +3,10 @@
 a = 1
 b = 2
 
-from add_0 import sum
+from add_0 import add
 
 if __name__== "__main__":
 
-    result = sum(a,b)
+    result = add(a,b)
     print("{}+{}={}".format(a,b, result))
     
