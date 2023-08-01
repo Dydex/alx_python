775ac6fc79830d8503c35897222a5db3e2174e72
Ajala Dolapo <supremeajala@gmail.com>
2023-08-01 19:11:30 +0100

    fourth task

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -6,7 +6,7 @@ def safe_print_division(a, b):
         result = None
     finally:
             print(" Inside result:{}".format(result))
-            print("{} / {} = {}".format(a, b, result))
+            
     return result
 
 safe_print_division(10, 2)
