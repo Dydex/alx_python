2a3d8242c61359a95114662cdc91cd3a7e28c2ba
Ajala Dolapo <supremeajala@gmail.com>
2023-08-01 18:51:53 +0100

    fourth task

---
+++ python-import_modules/3-safe_print_division.py
@@ -0,0 +1,12 @@
+def safe_print_division(a, b):
+
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print(" Inside result:{}".format(result))
+        print("{} / {} = {}".format(a, b, result))
+    return result
+
+safe_print_division(10, 2)
