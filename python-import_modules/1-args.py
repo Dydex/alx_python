7554a547f6b155ae95a61b3fcf1dcb5a7532e92e
Ajala Dolapo <supremeajala@gmail.com>
2023-08-01 13:49:39 +0100

    second task

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,21 @@
+import sys
+
+def main():
+    argv = sys.argv[1:]  # Exclude the script name itself from the arguments
+    num_args = len(argv)
+    
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+    else:
+        print(f"{num_args} arguments:")
+    
+    if num_args == 0:
+        print(".")
+    else:
+        for i, arg in enumerate(argv, start=1):
+            print(f"{i}: {arg}")
+
+if __name__ == "__main__":
+    main()
