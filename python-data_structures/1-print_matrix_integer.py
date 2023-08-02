65a83d80f260681c46eaf2c5dd65209284bc04e9
Ajala Dolapo <supremeajala@gmail.com>
2023-08-02 18:38:47 +0100

    second task

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,12 +1,9 @@
 def print_matrix_integer(matrix=[[]]):
-    # Get the number of rows and columns in the matrix
-    num_rows = len(matrix)
-    num_cols = len(matrix[0]) if matrix else 0
-
-    # Iterate through each row and print the elements separated by tabs
     for row in matrix:
-        for col_index, value in enumerate(row):
-            # Print the integer value with tab separation using str.format()
-            print("{:d}".format(value), end=" ")
-            print()
+        print()
+        for i in range(0, len(row)):
+            if i == len(row) - 1:
+                print("{:d}".format(row[i]))
+            else:
+                print("{:d}".format(row[i]), end=" ")
 
