f10ac0f1008bb3c2740ef13d3f9225735252423e
Ajala Dolapo <supremeajala@gmail.com>
2023-08-02 21:00:14 +0100

    second task

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,10 +1,10 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i in range(0, len(row)):
-            if i == len(row) - 1:
+        for column in row:
+            if column != row[-1]:
                 print("{:d}".format(row[i]), end=' ')
             else:
-                print("{:d}".format(row[i]), end=' ')
+                print("{:d}".format(row[i]), end='')
         print()
 
 
