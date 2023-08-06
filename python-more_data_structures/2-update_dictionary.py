acb316ac970ec0f2d4ecae8606abe6f9f95d7c37
Ajala Dolapo <supremeajala@gmail.com>
2023-08-06 09:52:52 +0100

    third task

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,37 +1,5 @@
-#a = 10
-#b = 5
-#sum = a + b
-#print("The sum is:", sum)
-
-#TOWN = "idi araba"
-#print("My Town is:", TOWN)
-
-#football = "chelsea"
-#print("MY FAV CLUB IS:", football)
-
-#TOWN = football
-#print(TOWN)
-# number = int(input("pls enter ur school name: "))
-# print("This is an", type(number) )
-
-# a = 1056774555476
-# result = a % 10
-# print("Answer:", result )
-
-# number = 5
-# print(number<10)
-
-# language = "python"
-# print("1",language == "python")
-
-# class Animal:
-#     pass
-
-my_list = [1,2,3,4,5]
-result = [x * 2 for x in my_list if x % 2 == 0]
-print(result)
-"""number1 = 7
-print(type(number1))
-
-number = 6.0
-print(type(number))"""
+def update_dictionary(a_dictionary, key, value):
+    if key in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        a_dictionary[key] = value
