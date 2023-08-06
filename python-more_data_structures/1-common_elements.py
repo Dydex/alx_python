06cb1c503c562448f548a9df52f4e87d5b05e5aa
Ajala Dolapo <supremeajala@gmail.com>
2023-08-06 08:33:17 +0100

    second task

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -3,7 +3,7 @@ def common_elements(a, b):
     set_2 = set (b)
     
     if (set_1 & set_2):
-        print(set_1 & set_2)
+        print(list(set_1 & set_2))
     else:
         print ("no common elemnts")
     
