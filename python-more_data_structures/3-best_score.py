3fcc8a5a7184597e420f46831442bad52886e329
Ajala Dolapo <supremeajala@gmail.com>
2023-08-07 14:57:19 +0100

    fourth task

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -2,11 +2,11 @@ def best_score(a_dictionary):
     if not  a_dictionary:
         return None
     
-    key = value
+    best_key = value
     best_score = int
 
     for key, value in a_dictionary:
         if value > best_score:
-            return key
+            return best_key
 
     
