df6acc4670ea4e77a40dd5c75104fd9e3874bfc3
Ajala Dolapo <supremeajala@gmail.com>
2023-08-08 13:34:09 +0100

    second task

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -9,7 +9,7 @@ class Square:
         Args:
             size (int): The size of the square.
         """
-        if size is not int:
+        if type(size) is not int:
             raise TypeError ("size must be an integer")
         elif size < 0:
             raise ValueError ("size must be >= 0")
