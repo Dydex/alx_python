ebc1cc03f58d019412b71b6c229bdca678159e97
Ajala Dolapo <supremeajala@gmail.com>
2023-08-08 01:53:18 +0100

    task

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,11 +1,11 @@
 """ This is a square(object) module.  """
 class Sqaure:
-    """ A class representing a square
+    """ A class representing a Square.
     
     Attributes
     ----
     __size = str
-        size of the square
+        size of the square.
 
     """ 
     pass
