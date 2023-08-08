db2fb78a7793de4907e1f396158088606022cdad
Ajala Dolapo <supremeajala@gmail.com>
2023-08-08 14:43:56 +0100

    fourth task

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -9,17 +9,21 @@ class Square:
         Args:
             size (int): The size of the square.
         """
-        if type(size) is not int:
+        self.__size = size  
+    
+    @property
+    def size(self):
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
             raise TypeError ("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError ("size must be >= 0")
     
-        self.__size = size
-    
+        self.__size = value
+
     def area(self):
         area = self.__size * self.__size
         return area
-    
-    @property
-    def get_size(self):
-        return self.__size
