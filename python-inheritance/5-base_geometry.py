f004d975f09ec97ce3cae8696009ab0ac565ec51
Ajala Dolapo <supremeajala@gmail.com>
2023-08-09 19:16:46 +0100

    sixth task

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -30,7 +30,7 @@ class BaseGeometry(metaclass=BaseMetaClass):
         def integer_validator(self, name, value):
             self.name = name
             if type(value) is not int:
-                raise TypeError ("name must be an integer")
+                raise TypeError ("age must be an integer")
             if value <= 0:
                 raise ValueError ("age must be greater than 0")
                 
