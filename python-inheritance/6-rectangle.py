56858341d49903a6c3e1de4fa3fa743dd2a6b93e
Ajala Dolapo <supremeajala@gmail.com>
2023-08-09 20:29:28 +0100

    seventh task

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -39,11 +39,8 @@ class Rectangle(BaseGeometry):
     This is a class Rectangle
     """
     def __init__(self, width, height):
-        self.__width = 0
-        self.__height = 0
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
         
-print(issubclass(Rectangle, BaseGeometry))
