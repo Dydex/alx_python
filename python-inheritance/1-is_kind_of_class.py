4d2b58191dfeaacbc17076e8fe4f2985ba78b54a
Ajala Dolapo <supremeajala@gmail.com>
2023-08-09 15:19:02 +0100

    third task

--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -3,6 +3,6 @@ This is function module .
 """
 def is_kind_of_class(obj, a_class):
         """
-        This function returns true/false if obj is exactly an instance of the specified class.
+        This function returns true/false if obj is exactly an instance of a class that inherits from the specified class.
         """
         return  isinstance(obj, a_class)
