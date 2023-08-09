35fbb91b9dbb19cdb2735ad546c4535dfe2d4069
Ajala Dolapo <supremeajala@gmail.com>
2023-08-09 19:10:16 +0100

    sixth task

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -27,10 +27,10 @@ class BaseGeometry(metaclass=BaseMetaClass):
         def area(self):
             raise Exception ("area() is not implemented")
 
-        def inter_validator(self, name, value):
+        def integer_validator(self, name, value):
+            self.name = name
             if type(value) is not int:
-                raise TypeError ("{} must be an integer".format(name))
-            elif value <= 0:
-                raise ValueError ("{} must be geater than 0".format(name))
-            else:
-                return value
+                raise TypeError ("must be an integer",format(name))
+            if value <= 0:
+                raise ValueError ("name must be geater than 0",format(name))
+                
