4d55c0a35cc460047b28d6eb994ed878edeed9ba
Ajala Dolapo <supremeajala@gmail.com>
2023-08-10 17:57:55 +0100

    seventh task

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -44,18 +44,17 @@ class BaseGeometry:
              return [x for x in attributes if x != '__init_subclass__']
                 
 class Rectangle(BaseGeometry):
-    """
-    This is a class Rectangle
-    """
+        """
+        This is a class Rectangle
+        """
     
-    def __init__(self, width, height):
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
-        self.__width = width
-        self.__height = height
+        def __init__(self, width, height):
+            super().integer_validator("width", width)
+            super().integer_validator("height", height)
+            self.__width = width
+            self.__height = height
         
-
-    def __dir__(cls) -> None:
-        attributes = super().__dir__()
-        return [x for x in attributes if x != '__init_subclass__']
+        def __dir__(cls) -> None:
+            attributes = super().__dir__()
+            return [x for x in attributes if x != '__init_subclass__']
     
