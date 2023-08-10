db952bc069ef6a5dbc8271248c75391982bf7f11
Ajala Dolapo <supremeajala@gmail.com>
2023-08-10 19:37:00 +0100

    last task

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -86,5 +86,11 @@ class Square(Rectangle):
         """ returns the area of the object."""
         return self.__size * self.__size
     
+    def __dir__(cls):
+            
+        """
+        This function removes the __init_subclass (method) from the default method inherited from the parent class
+        """
+        return [attribute for attribute in super().__dir__() if attribute != "__init_subclass__"]
 
     
