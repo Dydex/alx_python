cd4df23a5cd132fdb57ce6ab71925a2e19136e2a
Ajala Dolapo <supremeajala@gmail.com>
2023-08-11 23:07:37 +0100

    last task

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -19,3 +19,22 @@ class Square(Rectangle):
     def __str__(self):
         """ str magic method"""
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        """
+        getter method for width.
+        """
+        return self.__width
+
+    @size.setter
+    def size(self, value):
+        """
+        setter method for width.
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+
+        self.__width = value
