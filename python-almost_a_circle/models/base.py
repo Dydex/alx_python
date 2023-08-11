4d7a5538e82e5b7349f16c539ffb5b781b31c4f4
Ajala Dolapo <supremeajala@gmail.com>
2023-08-11 09:16:58 +0100

    first task

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -14,6 +14,6 @@ class Base:
         if id is not None:
             self.id = id
         else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects  
+            self.__class__.__nb_objects += 1
+            self.id = self.__class__.__nb_objects  
 
