67051ff10bf4749babf9f0bcce60fe47f6c47545
Ajala Dolapo <supremeajala@gmail.com>
2023-08-11 11:16:39 +0100

    first task

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -5,7 +5,7 @@ class Base:
     """
     This is a class Base
     """
-    ___nb_objects = 0
+    __nb_objects = 0
 
     def __init__(self, id=None):
         """
@@ -14,6 +14,7 @@ class Base:
         if id is not None:
             self.id = id
         else:
-            self.__class__.__nb_objects += 1
-            self.id = self.__class__.__nb_objects  
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects  
+            
 
