fef1563c2d29d62d7673013d23a1c53c5b81186e
Ajala Dolapo <supremeajala@gmail.com>
2023-08-11 22:58:11 +0100

    tenth task

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,21 @@
+"""
+This is a square module.
+"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    This is a square class.
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        This is the initialization method
+        """
+        super().__init__(size, size, x=0, y=0, id=None)
+
+    def __str__(self):
+        """ str magic method"""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
