6a0592c8fb6e22d7e6ef6fc68c7fd5c74154badc
Ajala Dolapo <supremeajala@gmail.com>
2023-08-12 10:33:28 +0100

    change

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -3,12 +3,13 @@ This is module for class Basegeometry.
 """
 
 BaseGeometry = __import__('5-base_geometry').BaseGeometry
-                
+
+
 class Rectangle(BaseGeometry):
     """
     This is a class Rectangle
     """
-    
+
     def __init__(self, width, height):
         """
         This is an integer validator method
@@ -17,12 +18,3 @@ class Rectangle(BaseGeometry):
         super().integer_validator("height", height)
         self.__width = width
         self.__height = height
-
-    def __dir__(cls):
-            """
-            This function removes the __init_subclass (method) from the default method inherited from the parent class
-            """
-            return [attribute for attribute in super().__dir__() if attribute != "__init_subclass__"]
-        
-        
-    
