b4d86e8576849508c37f62ce5a20becb55b9c793
Ajala Dolapo <supremeajala@gmail.com>
2023-08-13 12:01:24 +0100

    first task

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,11 +1,10 @@
-"""Module that fetches URL"""
+"""Module that fetches."""
 import requests
-"""Module that fetches URL"""
+
 url = "https://alu-intranet.htbn.io.status"
 response = requests.get(url)
-"""Module that fetches URL"""
+
 if response.status_code == 200:
-    """Module that fetches URL"""
     data = response.json()
     print("Body response:")
     print("\t- type: {}".format(type(data)))
