cfe23cf6af0609c8930990d35b6a31ee5bb55b2d
Ajala Dolapo <supremeajala@gmail.com>
2023-08-25 23:42:27 +0100

    first task

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+
 """This a Module that fetches the URL using requests package"""
 import requests
 """
@@ -8,15 +8,16 @@ response = requests.get("https://alu-intranet.htbn.io.status")
 """
 saves the requests in a variable
 """
-print("Body response:")
-"""
-prints body response
-"""
-print("\t- type: {}".format(type(response.text)))
-"""
-prints type of response text
-"""
-print("\t- content: {}".format(response.text))
-"""
-prints content of response text
-"""
+if __name__ == "__main__":
+    print("Body response:")
+    """
+    prints body response
+    """
+    print("\t- type: {}".format(type(response.text)))
+    """
+    prints type of response text
+    """
+    print("\t- content: {}".format(response.text))
+    """
+    prints content of response text
+    """
