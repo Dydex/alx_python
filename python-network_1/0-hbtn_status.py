31806a8bc63bd10f0003c0aefd233962338b78fa
Ajala Dolapo <supremeajala@gmail.com>
2023-08-25 12:53:59 +0100

    first task

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -4,10 +4,6 @@ import requests
 url = "https://alu-intranet.htbn.io.status"
 response = requests.get(url)
 
-if response.status_code == 200:
-    data = response.json()
-    print("Body response:")
-    print("\t- type: {}".format(type(data)))
-    print("\t- content: {}".format(data))
-else:
-    print("Failed to fetch data. Status code: {}".format(response.status_code))
+print("Body response:")
+print("\t- type: {}".format(type(response.text)))
+print("\t- content: {}".format(response.text))
