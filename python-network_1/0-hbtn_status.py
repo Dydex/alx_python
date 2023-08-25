07565b116beecbaadc4dd862f29eb706ffaaed62
Ajala Dolapo <supremeajala@gmail.com>
2023-08-25 23:36:23 +0100

    first task

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -4,21 +4,19 @@ import requests
 """
 This imports the requests method
 """
-if __name__ == "__main__":
-
-    response = requests.get("https://alu-intranet.htbn.io.status")
-    """
-    saves the requests in a variable
-    """
-    print("Body response:")
-    """
-    prints body response
-    """
-    print("\t- type: {}".format(type(response.text)))
-    """
-    prints type of response text
-    """
-    print("\t- content: {}".format(response.text))
-    """
-    prints content of response text
-    """
+response = requests.get("https://alu-intranet.htbn.io.status")
+"""
+saves the requests in a variable
+"""
+print("Body response:")
+"""
+prints body response
+"""
+print("\t- type: {}".format(type(response.text)))
+"""
+prints type of response text
+"""
+print("\t- content: {}".format(response.text))
+"""
+prints content of response text
+"""
