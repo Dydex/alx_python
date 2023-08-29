66dd6c113608fcde75ae8721a015fbf84de2b739
Ajala Dolapo <supremeajala@gmail.com>
2023-08-29 01:55:51 +0100

    second task

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+"""
+This module sends a request to the URL 
+and displays the value of the variable X-Request-Id
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    r = requests.get(url)
+    r1 = r.headers["X-Request-Id"]
+    print(r1)
