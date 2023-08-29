68ccac9e29bf96e220df1cffb19c7f7978323328
Ajala Dolapo <supremeajala@gmail.com>
2023-08-29 02:20:29 +0100

    second task

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -7,24 +7,7 @@ import sys
 
 if __name__ == "__main__":
 
-    def get_x_request_id(url):
-        try:
-            response = requests.get(url)
-            if response.status_code == 200:
-                x_request_id = response.headers.get('X-Request-Id')
-                if x_request_id:
-                    print(f'X-Request-Id: {x_request_id}')
-                else:
-                    print('X-Request-Id not found in the response headers.')
-            else:
-                print(
-                    f'Request failed with status code: {response.status_code}')
-        except requests.exceptions.RequestException as e:
-            print(f'An error occurred: {e}')
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <URL>")
-    else:
-        url = sys.argv[1]
-        get_x_request_id(url)
+    url = sys.argv[1]
+    r = requests.get(url)
+    r1 = r.headers["X-Request-Id"]
+    print(r1)
