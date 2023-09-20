6f0880e8be7e0f1b4b4ecd341c30642a351cbd1d
Ajala Dolapo <supremeajala@gmail.com>
2023-09-21 00:56:18 +0100

    eight task

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
-import sys
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print('Use: {} <mysql_username> <mysql_password> <database_name>'.format(
+        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(
             sys.argv[0]))
         sys.exit(1)
 
@@ -15,14 +15,20 @@ if __name__ == '__main__':
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(mysql_username, mysql_password, database_name),
-                           pool_pre_ping=True)
-    Session = sessionmaker(bing=engine)
+    # Create a database connection
+    engine = create_engine(
+        f"mysql+mysqldb://{mysql_username}:{mysql_password}@localhost:3306/{database_name}",
+        pool_pre_ping=True
+    )
+
+    # Create a session
+    Session = sessionmaker(bind=engine)
     session = Session()
 
+    # Query and print all State objects, sorted by states.id
     states = session.query(State).order_by(State.id).all()
     for state in states:
-        print('{}: {}'.format(state.id, state.name))
+        print(f"{state.id}: {state.name}")
 
+    # Close the session
     session.close()
