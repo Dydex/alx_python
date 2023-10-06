baab7f5f7111fe34e0650a5aa07fa3d725175825
Ajala Dolapo <supremeajala@gmail.com>
2023-10-06 02:37:25 +0100

    first  task

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -22,10 +22,10 @@ def get_employee_todo_progress(employee_id):
     print(
         f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
 
-    # Print titles of completed tasks with correct formatting
-    for i, task in enumerate(todo_data, start=1):
+    # Print titles of completed tasks
+    for task in todo_data:
         if task["completed"]:
-            print(f"Task {i} Formatting: OK")
+            print(f"    {task['title']}")
 
 
 if __name__ == "__main__":
