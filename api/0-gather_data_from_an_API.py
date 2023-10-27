861a169b93d50142534695ad002ab09c87c93d4a
Ajala Dolapo <supremeajala@gmail.com>
2023-10-27 01:17:55 +0100

    first task

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,28 +1,29 @@
+"""
+This module get data from todo api and writes the data to a csv file
+"""
+
 import json
 import requests
 import sys
 
 if __name__ == "main":
-    employee_id = sys.argv[1]
+    todo_url = f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}/todos'
 
-    response_user = requests.get(
-        f"https://jsonplaceholder.typicode.com/users/%7Bemployee_id%7D")
-    response_todos = requests.get(
-        f"https://jsonplaceholder.typicode.com/users/%7Bemployee_id%7D/todos")
+    req = requests.get(todo_url)
+    results = json.loads(req.text)
 
-    if response_user.status_code == 200 and response_todos.status_code == 200:
-        user_data = json.loads(response_user.text)
-        todos_data = json.loads(response_todos.text)
+    task = 0
+    completed_task_title = []
+    user_name = ''
+    for result in results:
+        if result['completed'] == True:
+            task = task + 1
+            completed_task_title.append(result['title'])
+        users = requests.get(
+            'https://jsonplaceholder.typicode.com/users/{}'.format(result['userId']))
+        user = json.loads(users.text)
+        user_name = user['name']
 
-        employee_name = user_data.get("name")
-        total_tasks = len(todos_data)
-        completed_tasks = sum(1 for task in todos_data if task["completed"])
-
-        print(
-            f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
-
-        for task in todos_data:
-            if task["completed"]:
-                print(f"\t {task['title']}")
-    else:
-        print("Failed to retrieve data. Please check the employee ID and API availability.")
+print('Employee {} is done with tasks({}/{}):'.format(user_name, task, len(results)))
+for finished_task in completed_task_title:
+    print('\t {}'.format(finished_task))
