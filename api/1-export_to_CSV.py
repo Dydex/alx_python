b623a75676f0150c240c830c2c98e06978384782
Ajala Dolapo <supremeajala@gmail.com>
2023-10-27 01:33:15 +0100

    second task

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -3,25 +3,25 @@ import json
 import requests
 import sys
 
-if __name__ == "main":
-    employee_id = sys.argv[1]
-    api_request = requests.get(
-        "https://jsonplaceholder.typicode.com/users/%7B%7D".format(employee_id))
-    api_request1 = requests.get(
-        "https://jsonplaceholder.typicode.com/users/%7B%7D/todos".format(employee_id))
-    data = api_request.text
-    pjson = json.loads(data)
-    data1 = api_request1.text
-    pjson1 = json.loads(data1)
 
-    # export data to csv data
-    filename = "{}.csv".format(employee_id)
-    with open(filename, 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        for item in pjson1:
-            user_id = employee_id
-            username = pjson['username']
-            task_completed_status = item['completed']
-            task_title = item['title']
-            writer.writerow(
-                [user_id, username, task_completed_status, task_title])
+employee_id = sys.argv[1]
+api_request = requests.get(
+    "https://jsonplaceholder.typicode.com/users/%7B%7D".format(employee_id))
+api_request1 = requests.get(
+    "https://jsonplaceholder.typicode.com/users/%7B%7D/todos".format(employee_id))
+data = api_request.text
+pjson = json.loads(data)
+data1 = api_request1.text
+pjson1 = json.loads(data1)
+
+# export data to csv data
+filename = "{}.csv".format(employee_id)
+with open(filename, 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+    for item in pjson1:
+        user_id = employee_id
+        username = pjson['username']
+        task_completed_status = item['completed']
+        task_title = item['title']
+        writer.writerow(
+            [user_id, username, task_completed_status, task_title])
