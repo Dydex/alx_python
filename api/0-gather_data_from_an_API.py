36c9910ddf4804ff3b5222fb076beed1467c1142
Ajala Dolapo <supremeajala@gmail.com>
2023-10-27 01:27:03 +0100

    first task

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,23 +6,22 @@ import json
 import requests
 import sys
 
-if __name__ == "main":
-    todo_url = f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}/todos'
+todo_url = f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}/todos'
 
-    req = requests.get(todo_url)
-    results = json.loads(req.text)
+req = requests.get(todo_url)
+results = json.loads(req.text)
 
-    task = 0
-    completed_task_title = []
-    user_name = ''
-    for result in results:
-        if result['completed'] == True:
-            task = task + 1
-            completed_task_title.append(result['title'])
-        users = requests.get(
-            'https://jsonplaceholder.typicode.com/users/{}'.format(result['userId']))
-        user = json.loads(users.text)
-        user_name = user['name']
+task = 0
+completed_task_title = []
+user_name = ''
+for result in results:
+    if result['completed'] == True:
+        task = task + 1
+        completed_task_title.append(result['title'])
+    users = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(result['userId']))
+    user = json.loads(users.text)
+    user_name = user['name']
 
 print('Employee {} is done with tasks({}/{}):'.format(user_name, task, len(results)))
 for finished_task in completed_task_title:
